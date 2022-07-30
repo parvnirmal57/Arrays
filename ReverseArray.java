2e18d68f62cbf5e4dea8ac1bac45aca5b0693d25
Parv Nirmal <110054722+parvnirmal57@users.noreply.github.com>
2022-07-30 18:51:59 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,21 @@
+package com.parv;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        reverseArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void reverseArray(int[] arr){
+        int n = arr.length;
+        for (int i=0;i<n/2;i++){
+            int temp = arr[i];
+            arr[i] = arr[n-1-i];
+            arr[n-1-i] = temp;
+
+        }
+    }
+}
