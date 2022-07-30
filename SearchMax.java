f586733992c4bfac8bd92b6c1e79a8c72f57c853
Parv Nirmal <110054722+parvnirmal57@users.noreply.github.com>
2022-07-30 18:50:34 +0530

    Update SearchMax.java

--- SearchMax.java
+++ SearchMax.java
@@ -1,5 +1,7 @@
 
-
+// Apporch: First of all we will assume maximum value element at 0th index position 
+// to check greater value we have to traverse whole array by applying condtion 
+// we will do comparsion of element then we will print element which has max value in array.
 public class SearchMax {
     public static void main(String[] args) {
         int[] a={4,3,5,2,1,6};
